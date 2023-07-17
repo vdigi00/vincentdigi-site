5ecf58bcc3ee9b5f16037b6038068b54dc3e18a5
vdigi00 <vinced66@gmail.com>
2023-07-17 18:05:02 -0400

    Removed boilerplate and added test

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+export default function Welcome() {
+    return (
+    <>
+    <div className="wrapper">
+    <h1>Test</h1>
+    <p>Hello World! Welcome to my App!</p>
+    </div>
+    </>
+    );
+    }

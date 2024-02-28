64ea23d64267c67e7ba55fa582d957f86eaa3e37
vdigi00 <vinced66@gmail.com>
2024-02-28 11:43:22 -0500

    Cleaned up skills using Tailwind class

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,82 +13,82 @@ const Skills = () => {
             <div className='max-w-[1000px] m-auto md:px-6 p-4 py-16 '>
                 <h1 className='lg:text-4xl text-3xl font-bold pb-6 font-LT-Hoop-Bold'>Skills:</h1>
                 <div className='flex flex-wrap justify-center lg:gap-6 gap-4 lg:text-xl text-lg'>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <FaGitAlt size={35}/>
                         <span className='pl-3'>Git</span>
                     </div>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <GrVmware size={35} />
                         <span className='pl-3'>VMWare</span>
                     </div>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <SiGnubash size={35} />
                         <span className='pl-3'>Bash</span>
                     </div>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <FaPython size={35} />
                         <span className='pl-3'>Python</span>
                     </div>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <FaJava size={35} />
                         <span className='pl-3'>Java</span>
                     </div>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <FaHtml5 size={35} />
                         <span className='pl-3'>HTML</span>
                     </div>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <FaCss3Alt size={35} />
                         <span className='pl-3'>CSS</span>
                     </div> 
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <SiJavascript size={35} />
                         <span className='pl-3'>JavaScript</span>
                     </div>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <SiVite size={35} />
                         <span className='pl-3'>Vite</span>
                     </div>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <SiTailwindcss size={35} />
                         <span className='pl-3'>TailwindCSS</span>
                     </div>
                 </div>
                 <h1 className='lg:text-4xl text-3xl font-bold py-6 font-LT-Hoop-Bold'>Interests:</h1>
                 <div className='flex flex-wrap justify-center lg:gap-6 gap-4 lg:text-xl text-lg'>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <GiSolderingIron size={35} />
                         <span className='pl-3'>Soldering</span>
                     </div>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer text-base font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <HiMiniWrenchScrewdriver size={35} />
                         <span className='pl-3'>Electronics Repair</span>
                     </div>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <FaDraftingCompass size={35} />
                         <span className='pl-3'>CAD</span>
                     </div>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <img src='../src/assets/images/printer.svg' className='w-8 h-auto'></img>
                         <span className='pl-3'>3D Printing</span>
                     </div>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <FaMusic size={35} />
                         <span className='pl-3'>Music</span>
                     </div>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <FaGuitar size={35} />
                         <span className='pl-3'>Guitar</span>
                     </div>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <FaTent size={35} />
                         <span className='pl-3'>Camping</span>
                     </div>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <FaHiking size={35} />
                         <span className='pl-3'>Hiking</span>
                     </div>
-                    <div className='border flex lg:basis-skills-lg md:basis-skills-md basis-skills-sm items-center align-middle justify-center p-4 cursor-pointer font-LT-Hoop-SemiBold'>
+                    <div class='skills-item'>
                         <IoBarbell size={35} />
                         <span className='pl-3'>Fitness</span>
                     </div>

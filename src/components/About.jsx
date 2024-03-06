cc6d35dd4416650d90ab954205680930bc9ef54a
vdigi00 <vinced66@gmail.com>
2024-03-06 02:23:43 -0500

    small content update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { FaFileDownload } from "react-icons/fa";
 
 const About = () => {
     return (
         <div id='about' className='min-h-[50em] cursor-default'>
             <div className='max-w-[1000px] m-auto flex flex-col p-8 py-16'>
                 <h1 data-aos='fade-right' className='text-4xl font-bold flex-1'>About Me:</h1>
-                <p data-aos='fade-right' data-aos-delay='500' className='flex-wrap text-xl font-LT-Hoop-SemiBold pt-5'>My name is Vincent DiGiovanni, I am a student at Rochester Institute of Technology, studying Computing and Information Technologies.</p>
-                <p data-aos='fade-right' data-aos-delay='500' className='text-xl font-LT-Hoop-SemiBold pt-5'>My concentration focuses on Networking, specifically Network Services & Protocols, Routing and Switching, Wireless Networking, and Network Design and Performance.</p>
-                <p data-aos='fade-right' data-aos-delay='500' className='text-xl font-LT-Hoop-SemiBold pt-5'>I am passionate about all things computers, electronics, and I love to learn.</p>
+                <div data-aos='fade-right' data-aos-delay='500' className='text-xl font-LT-Hoop-SemiBold'>
+                    <p className='pt-4'>My name is Vincent DiGiovanni, I am a student at Rochester Institute of Technology, studying Computing and Information Technologies.</p>
+                    <p className='pt-4'>My concentration focuses on Networking, specifically Network Services & Protocols, Routing and Switching, Wireless Networking, and Network Design and Performance.</p>
+                    <p className='pt-4'>I am passionate about all things computers, electronics, and I love to learn.</p>
+                    <h3 className='pt-8 text-xl font-LT-Hoop-Bold'>My resume is available for download below:</h3>
+                    <a className='bg-blue-500 text-white min-w-[250px] max-w-[400px] min-h-[50px] py-4 my-2 flex justify-center items-center hover:bg-blue-800 ease-in duration-100' href='/files/DiGiovanniV-Resume-2024.pdf'>
+                        <FaFileDownload size={30}/>
+                        <span className='pl-4'>Download Resume</span>
+                    </a>
+                </div>
             </div>
         </div>
     )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,7 @@ const Projects = () => {
         <div id='projects' className='bg-gray-100 min-h-[50em] cursor-default'>
             <div className='max-w-[1000px] m-auto p-8 py-16'>
                 <h1 data-aos='fade-right' className='text-4xl font-bold'>Projects:</h1>
+                <p className='mt-6 p-8 font-LT-Hoop-Bold text-2xl bg-yellow-200'>Content coming soon! (As of 3/5/24)</p>
             </div>
         </div>
     )

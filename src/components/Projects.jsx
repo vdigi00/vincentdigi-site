9737f6f508ee21a55699d40a816abb0d23c34248
vdigi00 <vinced66@gmail.com>
2024-03-18 11:07:24 -0400

    Testing staging branch

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,27 +4,26 @@ const Projects = () => {
     return (
         <div id='projects' className='bg-gray-100 min-h-[50em] cursor-default'>
             <div className='max-w-[1000px] m-auto p-8 pt-6'>
-                <h1 data-aos='fade-right' className='text-4xl font-MADE-TOMMY-Medium flex py-8'>Projects:</h1>
-                <p className='mt-6 p-8 font-MADE-TOMMY-Medium text-2xl bg-yellow-200'>Content coming soon! (As of 3/5/24)</p>
-                {/* <div className='text-xl font-LT-Hoop-Regular outline outline-1'>
+                <h1 data-aos='fade-right'>Projects:</h1>
+                <div className='text-xl outline outline-1'>
                     <h3>Personal Website</h3>
                     <div>
 
                     </div>
                 </div>
                 <h2>Other:</h2>
-                <div className='text-xl font-LT-Hoop-Regular outline outline-1'>
+                <div className='text-xl outline outline-1'>
                     <h3>Fuzz Pedal</h3>
                     <div>
 
                     </div>
                 </div>
-                <div className='text-xl font-LT-Hoop-Regular outline outline-1'>
+                <div className='text-xl outline outline-1'>
                     <h3>3D Printed Planes</h3>
                     <div>
 
                     </div>
-                </div> */}
+                </div>
             </div>
         </div>
     )

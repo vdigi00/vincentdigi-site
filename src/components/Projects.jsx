2ae75d6ca86d67e6c9e0851396e7195354677771
vdigi00 <vinced66@gmail.com>
2024-04-03 11:35:34 -0400

    Added fuzz pedal content

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,34 +6,47 @@ const Projects = () => {
             <div className='max-w-[1000px] m-auto px-8'>
                 <h1 data-aos='fade-right' data-aos-delay='500'>Projects:</h1>
                 <div className='text-xl'>
-                    <h3 className='text-2xl mb-5 font-MADE-TOMMY-Medium'>Personal Website</h3>
+                    <h3 className='text-2xl pb-5 font-MADE-TOMMY-Medium'>Personal Website</h3>
                     <swiper-container navigation='true' pagination='true' speed='500' loop='true'>
                         <swiper-slide><img src='/images/projects/web1.jpg' alt='Personal Website, Desktop Home Screen' loading="lazy" /></swiper-slide>
                         <swiper-slide><img src='/images/projects/web2.jpg' alt='Personal Website, Mobile Responsiveness, Home Screen' loading="lazy" /></swiper-slide>
                         <swiper-slide><img src='/images/projects/web3.jpg' alt='Personal Website, Mobile Responsiveness, Navigation menu' loading="lazy" /></swiper-slide>
                     </swiper-container>
                     <ul className='my-5 ml-8 list-disc'>
-                        <li>This site was developed using Vite/React, JavaScript, HTML, CSS, and TailwindCSS.</li>
-                        <li>Hosted on an AWS EC2 Linux Instance.</li>
-                        <li>Responsive design.</li>
+                        <li>This site was developed using Vite/React, JavaScript, HTML, CSS, and TailwindCSS</li>
+                        <li>Hosted on an AWS EC2 Linux Instance</li>
+                        <li>Responsive design</li>
                         <li>More content, changes, and optimizations coming soon...</li>
                         <li><a className='text-blue-500 underline' href='https://github.com/vdigi00/vincentdigi-site'>View GitHub Repository</a></li>
                     </ul>
                 </div>
                 <hr />
-                <div className='text-xl pb-5'>
+                <div className='text-xl py-5'>
                     <h3 className='text-2xl my-5 font-MADE-TOMMY-Medium'>Packet Analysis Tool</h3>
                     <ul className='my-5 ml-8 list-disc'>
-                        <li>Developed a Network Packet Analysis Tool using Python.</li>
-                        <li>Filtered Network Packet Captures, parsed hexadecimal data, and computed metrics.</li>
+                        <li>Developed a Network Packet Analysis Tool using Python</li>
+                        <li>Filtered Network Packet Captures, parsed hexadecimal data, and computed metrics</li>
                         <li>Effectively worked as a team to complete all three tasks on a schedule</li>
                         <li><a className='text-blue-500 underline' href='https://github.com/vdigi00/PacketAnalysis'>View GitHub Repository</a></li>
                     </ul>
                 </div>
-                {/*<div className='text-xl'>
-                    <h3>Fuzz Pedal</h3>
+                <hr />
+                <div className='text-xl py-5'>
+                    <h3 className='text-2xl my-5 font-MADE-TOMMY-Medium'>Fuzz Pedal</h3>
+                    <swiper-container navigation='true' pagination='true' speed='500' loop='true'>
+                        <swiper-slide><img src='/images/projects/fuzz1.jpg' alt='Schematic of Maestro Fuzz-Tone FZ-1A' loading="lazy" /></swiper-slide>
+                        <swiper-slide><img src='/images/projects/fuzz2.jpg' alt='Prototype fuzz circuit on breadboard' loading="lazy" /></swiper-slide>
+                        <swiper-slide><img src='/images/projects/fuzz3.jpg' alt='Soldering fuzz circuit onto perforated board' loading="lazy" /></swiper-slide>
+                        <swiper-slide><img src='/images/projects/fuzz4.jpg' alt='Fuzz circuit in 3D printed enclosure' loading="lazy" /></swiper-slide>
+                    </swiper-container>
+                    <ul className='my-5 ml-8 list-disc'>
+                        <li>Replica of Gibson Maestro Fuzz-Tone FZ-1A from 1966</li>
+                        <li>Hand soldered components on perforated board</li>
+                        <li>Designed enclosure using AutoDesk Inventor</li>
+                        <li>3D Printed using Creality Ender-3 V2</li>
+                    </ul>
                 </div>
-                <div className='text-xl'>
+                {/*<div className='text-xl'>
                     <h3>3D Printed Planes</h3>
                 </div>*/}
             </div>
